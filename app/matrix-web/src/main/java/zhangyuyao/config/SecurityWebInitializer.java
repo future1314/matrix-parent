a12547243a17e9f51a647bc0e26a5e3f4504989d
zhangyuyao <zhangyuyao@live.cn>
2018-10-13 22:19:41 +0800

    20181012

---
+++ app/matrix-web/src/main/java/zhangyuyao/config/SecurityWebInitializer.java
@@ -0,0 +1,6 @@
+//package zhangyuyao.config;
+//
+//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+//
+//public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
+//}
